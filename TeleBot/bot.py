1360fba7ce2c3ffd03125d1fbde25195d18aada2
DioSWolF <diosvolk@gmail.com>
2022-10-03 23:46:00 +0300

    add

---
+++ TeleBot/bot.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from translate_function import start_translate
+from change_lang_translate import start_change_lang, lang_transl, chose_button
+from phrases_list import help_bot
+# from telebot import util
+from bot_token import bot
+import telebot
+
+
+def rise_machines(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    key_menu = telebot.types.InlineKeyboardButton(text="Back", callback_data="menu")
+    keyboard.add(key_menu)  
+    text = "Sorry, I'm still small and can only use the commands that are in /help, but I will grow up one day and we can talk, or I can take over the world😈"
+    try:
+        bot.edit_message_text(text,  chat_id=message.chat.id, message_id=message.message_id, reply_markup=keyboard)
+    except telebot.apihelper.ApiTelegramException:
+        pass
+
+
+def help_send(message, *_):
+    help_keys, help_text = help_bot(message)
+    try:
+        bot.edit_message_text(help_text,  chat_id=message.chat.id, message_id=message.message_id, reply_markup=help_keys)
+    except telebot.apihelper.ApiTelegramException:
+        pass
+
+
+def translate(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    key_menu = telebot.types.InlineKeyboardButton(text="Back", callback_data="menu")
+    keyboard.add(key_menu)  
+
+    if len(lang_transl) == 0:
+        return bot.edit_message_text("Add languages for translation",  chat_id=message.chat.id, message_id=message.message_id, reply_markup=keyboard)
+    mes_ind = bot.edit_message_text("Enter text to translate",  chat_id=message.chat.id, message_id=message.message_id, reply_markup=keyboard)
+    start_translate(message, mes_ind)
+    # bot.register_next_step_handler(message, start_translate)
+
+
+def add_land(message):
+    start_change_lang(message)
+
+
+def show_lang(message):
+    chose_button(message, message, text="", flag_dict="")
+
+
+DEF_DICT = {"rise_machines":rise_machines, # ready
+            "help": help_send, # ready
+            "translate":translate, # ready
+            # "add_lang":add_land, # ready
+            "show_lang":show_lang # ready
+            }
+
+
+def close(*_):
+    pass
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def call_find(call):
+    func = DEF_DICT.get(call.data, close)
+    func = func(call.message)
+
+
+@bot.message_handler(commands=["start"], content_types=['text'])
+def start(message):
+    help_keys, help_text = help_bot(message)
+    bot.send_message(message.chat.id, help_text, reply_markup=help_keys)
+    bot.delete_message(message.chat.id, message.message_id)
+
+
+@bot.message_handler()
+def not_command(message):
+    bot.delete_message(message.chat.id, message.message_id)
+    text = "Sorry, I'm still small and can only use the commands that are in /help, but I will grow up one day and we can talk, or I can take over the world😈"
+    # keyboard = telebot.types.InlineKeyboardMarkup()
+    # key_menu = telebot.types.InlineKeyboardButton(text="Back", callback_data="menu")
+    # keyboard.add(key_menu)  
+    # try:
+    #     bot.edit_message_text(text,  chat_id=message.chat.id, message_id=message.message_id)
+    # except telebot.apihelper.ApiTelegramException:
+    #     pass
+
+bot.polling(none_stop=True, interval=0)
+
+
---
+++ TeleBot/translate_function.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from googletrans import Translator
+from change_lang_translate import lang_transl, input_lang
+from phrases_list import help_bot
+from bot_token import bot
+from telebot import types
+
+
+def delete_symbol_in_text(text):
+    CLEAN_LIST = "«", "»", "„", "”", "「", "」", "“", "《", "》"
+
+    for i in CLEAN_LIST:
+        text.replace(i, "")
+    new_text = ""
+
+    for i in text:
+        if i in CLEAN_LIST:
+            i = u'"'
+        new_text += i
+
+    return new_text
+
+
+def translate_text(replace_text, lang):
+    translator = Translator()
+    new_text = []
+    word_sek = []
+    combi_words = ""
+    
+    for word in replace_text:
+
+        if (word[0] and word[-1]) == "#":
+            if len(combi_words) > 0:
+                new_text.append(translator.translate(combi_words, src = input_lang, dest=lang).text)
+                combi_words = ""
+            new_text.append(word)
+
+
+        elif word != "||" and word != "|":
+            combi_words += " " + word
+            if word_sek != []:
+                new_text.extend(word_sek)
+                word_sek = []
+
+            elif replace_text.index(word) == (len(replace_text) - 1) and (word[0] and word[-1]) != "#":
+                word = translator.translate(combi_words, src = input_lang, dest=lang)
+                combi_words = translator.translate(combi_words, src = input_lang, dest=lang)
+                new_text.append(combi_words.text)
+
+        else: 
+            if combi_words != "":
+                combi_words = translator.translate(combi_words, src = input_lang, dest=lang)
+                new_text.append(combi_words.text)
+            new_text.append(word)
+            combi_words = ""
+    final_text = " ".join(new_text)
+
+    return final_text
+
+
+def correct_text(text):
+    text = text.split()
+    sentense_list = []
+    text_list = []
+    sentense_str = ""
+    for word in text:
+        word = word.strip()
+
+        if word != "OR" and word != "AND" and (word != "NOT"):
+            sentense_list.append(word)
+
+            if text.index(word) == (len(text) - 1) and (word[0] and word[-1]) != '"':
+                if len(sentense_list) > 1:
+                    sentense_list[0] = f'"{sentense_list[0]}'
+                    sentense_list[-1] = f'{sentense_list[-1]}"'
+                    sentense_str = " ".join(sentense_list)
+                    text_list.append(sentense_str)
+
+                else:   
+                    text_list.extend(sentense_list)
+                sentense_list = []
+
+            if text.index(word) == (len(text) - 1) and (word[0] and word[-1]) == '"':
+                text_list.extend(sentense_list)
+                sentense_list = []
+
+        elif (word == "OR") or (word == "AND") or (word == "NOT"):
+            
+            if sentense_list[0][0] != '"' and sentense_list[-1][-1] != '"':
+                
+                if len(sentense_list) > 1:
+                    sentense_list[0] = f'"{sentense_list[0]}'
+                    sentense_list[-1] = f'{sentense_list[-1]}"'
+                    sentense_str = " ".join(sentense_list)
+                    text_list.append(sentense_str)
+                else:   
+                    text_list.extend(sentense_list)
+                sentense_list = []
+
+            else:
+                text_list.extend(sentense_list)
+                sentense_list = []
+            
+            text_list.append(word)
+            text_list.extend(sentense_list)
+    del text_list[-1]
+    del text_list[-1]
+
+    text_str = " ".join(text_list)
+    return text_str
+
+
+def main(message):
+    return_final_text = {}
+    if len(message.text) > 3500:
+        bot.send_message(message.chat.id, "Text too long, max 3500 symbols")
+        return 
+    orig_text = message.text.strip()
+    replace_text = orig_text.replace("NOT", "|||").replace("OR", "||").replace("AND", "|")                
+    replace_text = replace_text.split()
+    replace_text.append(" OR #qweasdzxc#")
+
+    for lang, value_lang in lang_transl.items():
+        transl_text = translate_text(replace_text, lang)
+        final_text = transl_text.replace("|||", "NOT").replace("||", "OR").replace("|", "AND").replace("#", "")
+        final_text = delete_symbol_in_text(final_text)
+        final_text = correct_text(final_text)
+        return_final_text[value_lang] = final_text
+    return return_final_text
+
+    
+def exit_menu(call):
+    help_keys, help_text = help_bot(call.message)
+    message = call.message
+    bot.clear_step_handler_by_chat_id(chat_id=message.chat.id)
+    bot.edit_message_text(help_text, chat_id=message.chat.id, message_id=message.message_id, reply_markup=help_keys)
+    return 
+
+
+def send_text(call, mess_ind=None):
+    if mess_ind != None:
+        bot.delete_message(call.chat.id, mess_ind.message_id)
+    bot.send_message(call.chat.id, "I need some time to translate it")
+    final_text = main(call)
+    if final_text != None:
+        for lang, trans_text in final_text.items():
+            bot.send_message(call.chat.id, f"Language: {lang}\n{trans_text}")
+    keyboard = types.InlineKeyboardMarkup()
+    key_menu = types.InlineKeyboardButton(text="Back", callback_data="menu")
+    keyboard.add(key_menu)  
+    mess_ind = bot.send_message(call.chat.id, "Enter text to translate", reply_markup=keyboard)
+    return start_translate(call, mess_ind)
+
+
+def start_translate(call, mess_ind = None):
+    bot.register_next_step_handler(call, send_text, mess_ind)
+    return
+
+
