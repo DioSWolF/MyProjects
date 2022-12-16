ea418464b46b54f2486a049d66e913acba302664
DioSWolF <106728732+DioSWolF@users.noreply.github.com>
2022-12-17 00:45:14 +0200

    Update bot_token.py

--- SerialBot/bot_token.py
+++ SerialBot/bot_token.py
@@ -3,17 +3,15 @@ from telebot.asyncio_storage import StateMemoryStorage
 
 
 
-bot_id = 5604497270
+bot_id = ""
 token = ""      
 
 
 bot = telebot.async_telebot.AsyncTeleBot(token, state_storage=StateMemoryStorage())
 
-find_anime_link = f"https://animego.org/search/anime?q="
+find_anime_link = f"https://your_site.us/search/anime?q="
 find_anime_num_page = "&type=list&page="
-find_new_anime_link = "https://animego.org"
+find_new_anime_link = "https://your_site.ua"
 
-# save_image_folder = r"/home/diosvolk/save_data/image_list/"
-# save_file_folder = r"/home/diosvolk/save_data/save_bin_file/"
 save_image_folder = r"./image_list/"
 save_file_folder = r"./save_bin_file/"
