30f24587c6cb47aa78245572f548567a829e1f83
DioSWolF <106728732+DioSWolF@users.noreply.github.com>
2022-12-20 00:01:29 +0200

    Update bot_token.py

--- SerialBot/bot_token.py
+++ SerialBot/bot_token.py
@@ -13,4 +13,4 @@ find_anime_link = f"https://your_site.us/search/anime?q="
 find_anime_num_page = "&type=list&page="
 find_new_anime_link = "https://your_site.ua"
 
-save_image_folder = r"./image_list/"
+save_image_folder = r"./save_data/image_list/"
